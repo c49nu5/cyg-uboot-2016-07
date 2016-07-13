7836eace5e677ab8c956ed213b344ecbd93ad313
david george <david@cygnus-instruments.com>
2016-07-13 11:38:47 +0100

    Initial version

---
+++ mx6sxsabresd_config/include/config.h
@@ -0,0 +1,8 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/freescale/mx6sxsabresd/imximage.cfg
+#define CONFIG_BOARDDIR board/freescale/mx6sxsabresd
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/mx6sxsabresd.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ mx6sxsabresd_config/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,4 @@
+#define U_BOOT_DATE "Jul 13 2016"
+#define U_BOOT_TIME "11:31:34"
+#define U_BOOT_TZ "+0100"
+#define U_BOOT_DMI_DATE "07/13/2016"
